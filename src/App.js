800bc1fc7673307aa82f3a3e9d98fb03786c8ef3
PEDRO TORQUATO <leotorquato@edu.unifor.br>
2021-08-03 15:01:44 -0300

    add .env

--- src/App.js
+++ src/App.js
@@ -5,10 +5,11 @@ import 'react-toastify/dist/ReactToastify.css';
 
 import GlobalStyle from './styles/global';
 import Routes from './routes';
-
+import dotenv from 'dotenv'
 import AppProvider from './hooks';
-
+dotenv.config()
 function App() {
+  console.log(process.env.REACT_APP_WDS_SOCKET_PATH)
   return (
     <BrowserRouter>
       <ToastContainer autoClose={4000} position="top-right" />
--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-
+require('dotenv').config()
+console.log(process.env.WDS_SOCKET_PATH)
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
+  
 );
